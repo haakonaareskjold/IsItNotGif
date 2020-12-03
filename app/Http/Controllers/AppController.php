76bfdb07102e52a73ee3a5edbae51adc1eee7e05
Haakon <aareskjold.haakon@gmail.com>
2020-12-03 18:20:02 +0100

    added some if statements for if body has string containing either .gif or .mp4, or mimetype video/mp4 - image/gif, and checks byte sequence in beginning of file for known sequence for animations

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -22,11 +22,44 @@ class AppController extends Controller
     {
         $file = $request->input('url');
         $response = Http::get($file);
+        if(str_contains($response->body(), '.gif') || str_contains($response->body(), '.mp4')) {
+            return redirect('/')->with('danger', 'CAREFUL, it is animated');
 
-        if ($response->header('content-type') == 'image/gif') {
-            return redirect('/')->with('danger', 'YOUR LINK CONTAINS A GIF');
+        } elseif($response->header('content-type') == 'video/mp4' || $response->header('content-type' == 'image/gif')) {
+            return redirect('/')->with('danger', 'CAREFUL, it is animated');
+
+            // idea from function at php.net - https://www.php.net/manual/en/function.imagecreatefromgif.php
+        } elseif (is_string($file)) {
+            $fp = fopen($file, "rb");
         } else {
-            return redirect('/')->with('safe', 'It was not a gif!');
+            $fp = $file;
+
+            /* Make sure that we are at the beginning of the file */
+            fseek($fp, 0);
+        }
+
+        if (fread($fp, 3) !== "GIF") {
+            fclose($fp);
+
+            return redirect('/')->with('safe', 'it is NOT animated');
+        }
+
+        $frames = 0;
+
+        while (!feof($fp) && $frames < 2) {
+            if (fread($fp, 1) === "\x00") {
+                /* Some of the animated GIFs do not contain graphic control extension (starts with 21 f9) */
+                if (fread($fp, 1) === "\x21" || fread($fp, 2) === "\x21\xf9" || fread($fp, 2) === "\x21\x2c") {
+                    $frames++;
+                }
+            }
+        }
+
+        fclose($fp);
+
+        if($frames > 1) {
+            return redirect('/')->with('danger', 'CAREFUL, it is animated');
         }
     }
+
 }
